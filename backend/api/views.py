9bbeb9bee98c6a819819e34e3e82aefe32f8d6f6
GiorgiGiorgobianni <giorgobiani504@gmail.com>
2024-04-18 14:17:42 +0400

    added note delete view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_class = [IsAuthenticated]
+
+    def ger_queryset(self):
+        user = self.request.user
+        #returns authenticated user
+
+        return Note.objects.filter(author=user)
+        #get notes by a specific author
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class DeleteNote(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_class = [IsAuthenticated]
+
+    def get_queryset(self):#this function makes sure to let users delete notes that were created by themselves only
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
